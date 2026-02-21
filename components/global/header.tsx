ab120b48848e68f29b85c2c55ea5126b3a3f186f
Ser Foll <sergiofollyvi@gmail.com>
2026-02-21 20:04:22 +0100

    Added: basic header layout

---
+++ components/global/header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header className="p-6 sm:px-0 shadow">
+      <div className="center container mx-auto flex justify-between ">
+        <h1 className="text-xl font-bold sm:text-2xl">Where in the world?</h1>
+        <button type="button" className="cursor-pointer">
+          Dark Mode
+        </button>
+      </div>
+    </header>
+  );
+}
